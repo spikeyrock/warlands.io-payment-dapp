7a9d32f22fea49b711de6371cfd65a9ba3886257
Ritwik Chakravarty <88462061+spikeyrock@users.noreply.github.com>
2021-12-26 10:56:15 -0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,57 +1,60 @@
-
 // SPDX-License-Identifier: MIT
 // Author : spikeyrock - nftit
 // Contact : nftit@protonmail.com
+// Discription : Payment system to recieve funds for Warlands.io "Chests"
 
 
-pragma solidity =0.8.9;
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WarlandsChest {
+pragma solidity ^0.8.0;
+contract WarlandsChest is Ownable  {
+    string public name = "Warlands Presale Chests";
+    uint256 public SilverCost = 0.25 ether;
+    uint256 public totalSilverMinted;
+    uint256 public GoldCost = 0.65 ether;
+    uint256 public totalGoldMinted;
+    uint256 public DiamondCost = 1.65 ether;
+    uint256 public totalDiamondMinted;
 
-    uint256 public SilverCost = 0.1 ether;
-    uint256 public totalSilverMinted = 0;
-    uint256 public GoldCost = 0.3 ether;
-    uint256 public totalGoldMinted = 0;
-    uint256 public DiamondCost = 0.75 ether;
-    uint256 public totalDiamondMinted = 0;
     
-
-  
     event boughtChest(address indexed _from, uint256 cost); 
-    
     modifier shouldPay(uint256 _cost) {
         require(msg.value >= _cost, "The chests cost more!");
         _;
     }
-
     function BuySilverChest() payable public shouldPay(SilverCost) {
         emit boughtChest(msg.sender, SilverCost);
-        totalSilverMinted = totalSilverMinted + 1;
+        totalSilverMinted++;
     }
-
     function BuyGoldChest() payable public shouldPay(GoldCost) {
         emit boughtChest(msg.sender, GoldCost);
-        totalGoldMinted = totalGoldMinted + 1;
+        totalGoldMinted++;
     }
-
     function BuyDiamondChest() payable public shouldPay(DiamondCost) {
         emit boughtChest(msg.sender, DiamondCost);
-        totalDiamondMinted = totalDiamondMinted + 1;
+        totalDiamondMinted++;
     }
-
     function getFunds() public view returns(uint256) {
         return address(this).balance;
     }
-
-    address payable public owner;
-
-    constructor() payable {
-        owner = payable(msg.sender);
+   
+    function setCostSilver(uint256 _newCost) public onlyOwner {
+    SilverCost = _newCost;
     }
 
-    function withdraw() public {
-        uint amount = address(this).balance;
-        (bool success, ) = owner.call{value: amount}("");
-        require(success, "Failed to send Ether");
+    function setCostGold(uint256 _newCost) public onlyOwner {
+    GoldCost = _newCost;
     }
+
+    function setCostDiamond(uint256 _newCost) public onlyOwner {
+    DiamondCost = _newCost;
+    }
+  
+     function withdraw() public payable onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+  }
 }
+
